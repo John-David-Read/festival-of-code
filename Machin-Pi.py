0f40cf3d68c0a8dde1f0976058806ca0a42c07a3
John-David-Read <john.read@arup.com>
2024-08-07 10:11:07 +0100

    Changes updated

---
+++ Machin-Pi.py
@@ -0,0 +1,23 @@
+def itan(x,terms):
+    """
+    Sums terms of Taylor series of inverse tan of x.
+    """
+    series_sum = 0
+    for k in range(0,terms):
+        series_sum = series_sum + ((-1)**k * x**(2*k+1) / (2*k+1))
+    return series_sum
+
+terms = 4
+piapprox = 16 * itan(1/5,terms) - 4 * itan(1/239,terms)
+print("Approximation: {}.".format(piapprox))
+
+import math
+print("math.pi: {}.".format(math.pi))
+print("Error: {}.\n".format(math.pi - piapprox))
+
+for terms in range(1,11):
+    print("{} terms:".format(terms))
+    piapprox = 16 * itan(1/5,terms) - 4 * itan(1/239,terms)
+    print("Approximation: {}.".format(piapprox))
+    print("math.pi: {}.".format(math.pi))
+    print("Error: {}.\n".format(math.pi - piapprox))
