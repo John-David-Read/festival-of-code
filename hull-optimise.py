6caf2a660709faaa80d8140be509a6ac8326370b
John-David-Read <john.read@arup.com>
2024-08-30 13:40:45 +0100

    2024-08-30

--- hull-optimise.py
+++ hull-optimise.py
@@ -9,26 +9,31 @@ def polyhedron_volume(vertices):
     hull = sp.ConvexHull(vertices)
     return hull.volume
 
-# Constraint to ensure vertices lie on the unit sphere
-def constraint(vertices):
-    return np.sum(vertices**2, axis=1) - 1
+# Function to calculate a vertices matrix from a list of angles
+def angle_vertices(angle):
+    vert = np.empty((0,3))
+    for i in range(int(len(angle)/2)):
+        vert = np.vstack([vert,[cos(angle[2*i])*cos(angle[2*i+1]), sin(angle[2*i])*cos(angle[2*i+1]), sin(angle[2*i+1])]])
+    return(vert)
 
+# function to create n pairs of random angles from 0 - 2 pi
+def angle_random(n):
+    angle = []
+    for i in range(n):
+        theta = random.random() * 2 * pi
+        phi = asin(random.random() * 2 - 1)
+        angle.append(theta)
+        angle.append(phi)
+    return(angle)
+
+# function to return the polyhedron volume given the angles
+def volume(angle):
+    return (polyhedron_volume(angle_vertices(angle)))
+     
 # Initial guess for the vertices (random points on the unit sphere)
 n = 8  # Number of vertices
-vert = np.empty((0,3))
-
-for i in range(n):
-    theta = random.random() * 2 * pi
-    phi = asin(random.random() * 2 - 1)
-    vert = np.vstack([vert,[cos(theta)*cos(phi), sin(theta)*cos(phi), sin(phi)]])
-
-# Define the constraints and bounds
-constraints = {'type': 'eq', 'fun': constraint}
-bounds = [(-1, 1)] * len(vert)
-
-print(vert)
-print(len(vert))
-print(polyhedron_volume(vert))
+angle = angle_random(n)
+print(volume(angle))
 
 vm = np.empty((0,3))
 phi = acos(sqrt((15+sqrt(145))/40))
@@ -41,9 +46,12 @@ vm = np.vstack([vm,[0,-sin(phi),-cos(phi)]])
 vm = np.vstack([vm,[0,sin(phi),-cos(phi)]])
 vm = np.vstack([vm,[0,sin(3*phi),-cos(3*phi)]])
 
-print(vm)
-print(len(vm))
-print(polyhedron_volume(vm))
+#print(vm)
+#print(len(vm))
+#print(polyhedron_volume(vm))
+
+# Define the constraints and bounds
+#bounds = [(-1, 1)] * len(vert)
 
 # Optimize the volume
 #result = op.minimize(lambda x: -polyhedron_volume(x.reshape(-1, 3)), vert, constraints=constraints, bounds=bounds)
