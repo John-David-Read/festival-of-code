0351edfab8869078298aeaff26823af0f98a3540
John-David-Read <john.read@arup.com>
2024-09-05 17:43:11 +0100

    Update hull-optimise-once.py

--- hull-optimise-once.py
+++ hull-optimise-once.py
@@ -53,10 +53,10 @@ def compare(this_volume,local_maxima):
     return True
 
 #run for n and collect succesive maximum volumes
-n = 24
+n = 30
 local_maxima = []
 
-for i in range(1,100):
+for i in range(1,25):
     angle = angle_random(n)
     bounds = []
     bounds = [(0, 2*pi),(-pi/2,pi/2)] * n
